ee01b7247e8ed9c5dc0b200db1947ea35e2f0f9b
OEM Configuration (temporary user) <giorgicxvedadze4@gmail.com>
2022-07-14 12:49:39 +0400

    added an error message

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from './AvailableMeals.module.css';
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
 const AvailableMeals = () => {
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState();
 
-    const mealsList = DUMMY_MEALS.map(meal =>
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://meals-http-fc447-default-rtdb.firebaseio.com/meals.json');
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!')
+            }
+
+            const responseData = await response.json();
+
+            const loadMeals = [];
+
+            for (const key in responseData) {
+                loadMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                })
+            }
+
+            setMeals(loadMeals);
+            setIsLoading(false);
+        };
+
+        fetchMeals().catch((error) => {
+            setIsLoading(false);
+            setError(error.message);
+        });
+    }, [])
+
+    if (isLoading) {
+        return (
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className="d-flex justify-content-center">
+                <p className="text-danger text-uppercase fs-3 fw-normal">{error}</p>
+            </div>
+        )
+    }
+
+    const mealsList = meals.map(meal =>
         <MealItem
             key={meal.id}
             id={meal.id}
